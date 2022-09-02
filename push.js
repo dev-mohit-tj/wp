b8f8d608c4dab20232cead47c780af01f6e9e794
dev-mohit-tj <mohit.kumar@techjockey.com>
2022-09-02 18:12:07 +0530

    fixing it

--- push.js
+++ push.js
@@ -8,6 +8,6 @@ let vapidkeys = {
 
   push.setVapidDetails('mailto:mohit@tech.com',vapidkeys.publicKey, vapidkeys.privateKey);
 
-  let subscribed = {"endpoint":"https://fcm.googleapis.com/fcm/send/cb_Zcjm0i8E:APA91bHj7iPSlRkRB9lNcp1gkENXBcTsAedE81N1mpGizTwQlfNBYddz_o7BYPBL65x0RfsO13RAkR-wEI9cUUE4J-jfDbuj7Ivnrq1d6XDbPACOSXyKfsnaai6g2NUXHWSBpiY4M4EZ","expirationTime":null,"keys":{"p256dh":"BJL2zJlRqTekkRYW6oXCQd7FBXxPlBfUhbksHlbYg9_BgeeUWX1b4B5UDB4Aw1b4D6yPOkt4D5j_jMh8cZHlco0","auth":"68ISzqZbJwlGSz4LG_zxsw"}}; 
+  let subscribed = {endpoint:"https://fcm.googleapis.com/fcm/send/cb_Zcjm0i8E:APA91bHj7iPSlRkRB9lNcp1gkENXBcTsAedE81N1mpGizTwQlfNBYddz_o7BYPBL65x0RfsO13RAkR-wEI9cUUE4J-jfDbuj7Ivnrq1d6XDbPACOSXyKfsnaai6g2NUXHWSBpiY4M4EZ",expirationTime:null,keys:{p256dh:"BJL2zJlRqTekkRYW6oXCQd7FBXxPlBfUhbksHlbYg9_BgeeUWX1b4B5UDB4Aw1b4D6yPOkt4D5j_jMh8cZHlco0",auth:"68ISzqZbJwlGSz4LG_zxsw"}}; 
 
   push.sendNotification(subscribed, 'hello from mohit')
