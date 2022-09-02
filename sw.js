e72942a00570e8a2ca4d3f795ac1ed062dcba725
dev-mohit-tj <mohit.kumar@techjockey.com>
2022-09-02 18:02:44 +0530

    fetched endpoint of user

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
 self.addEventListener('push', ()=> {
-    self.registration.sendNotification('helo here', {})
+
+    var options = {
+        body: 'this is notification generated for you',
+        vibrate: [100,50,100],
+        data: {
+            dateOfArival: Date.now(),
+            primaryKey: '2',
+        },
+        actions: [
+            { action: 'explore', title : 'explore this world'},
+            { action: 'close', title: 'close this world'}, 
+        ]
+    }
+    e.waitUntil(
+    self.registration.showNotification("hii i am mohit here", options)
+    );
 });
