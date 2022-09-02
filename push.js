bbae061058c798f204a5c03c377e00071096b907
dev-mohit-tj <mohit.kumar@techjockey.com>
2022-09-02 18:54:12 +0530

    adding image to aviral sir

--- push.js
+++ push.js
@@ -9,11 +9,11 @@ let vapidkeys = {
   push.setVapidDetails('mailto:mohit@tech.com',vapidkeys.publicKey, vapidkeys.privateKey);
 
   let subscribed = {
-    endpoint:"https://fcm.googleapis.com/fcm/send/ecZplj2d8fQ:APA91bHMgDVpV-Adp2MrWnVeA4lteJhiBv9DmAaj7CLU4Zr2u6AGgWjKPzt-Qda1DferDO3deH_26oZZrZfYsf0xMPGevSrksqpYE8q1KMxFoPFkHvkuEA-98SQmIIvjGl0IVu5KCW7R",
+    endpoint:"https://fcm.googleapis.com/fcm/send/dSf1w4jPors:APA91bFTxPSMQey0mP03xfK7YhxTqClBdt1gw0KGmiH7P-V0k_ZPlm7PoLkLOz9pMIiZhqOn4STCoZj5CgkWZOZCNMfJAmhNUSkqsAp38rYvyt88uUyVtoSuu3jKNLx8rTRbxWL4Qz_j",
     expirationTime:null,
     keys:{
-        p256dh: "BIP_BAYYNbFF2HcTDjO_Fom2s7FMV0oMo-HX9cEzaQ1l1Z-8lfWMMKUTAbnEIEJ911B91QEU7_Dd7aqN-joDJe4",
-        auth:"Jdq5gpK-aMA7szUjndLJnw"
+        p256dh: "BLDdtota7RBEUaU0BCvhP47vAEhy3beEUJUvMHiQ_rq8adBqCKy0c6pKbHjELnZdqD_UNoeZrJcweY7lbFUx1EM",
+        auth:"nF70gc_ROohMUhpmZZRtQQ"
     }
     // {"endpoint":"https://fcm.googleapis.com/fcm/send/ecZplj2d8fQ:APA91bHMgDVpV-Adp2MrWnVeA4lteJhiBv9DmAaj7CLU4Zr2u6AGgWjKPzt-Qda1DferDO3deH_26oZZrZfYsf0xMPGevSrksqpYE8q1KMxFoPFkHvkuEA-98SQmIIvjGl0IVu5KCW7R","expirationTime":null,"keys":{"p256dh":"BIP_BAYYNbFF2HcTDjO_Fom2s7FMV0oMo-HX9cEzaQ1l1Z-8lfWMMKUTAbnEIEJ911B91QEU7_Dd7aqN-joDJe4","auth":"Jdq5gpK-aMA7szUjndLJnw"}}
   };
--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('push', function(e) {
 
     var options = {
         body: 'this is notification generated for you',
-        image: "https://media-exp1.licdn.com/dms/image/C5603AQGeLYdqFSPgGQ/profile-displayphoto-shrink_800_800/0/1616413983355?e=2147483647&v=beta&t=egTafUuzkeMApWlRv9l7P5tb-K-8jALz77FEv06PZww",
+        icon: "https://media-exp1.licdn.com/dms/image/C5603AQGeLYdqFSPgGQ/profile-displayphoto-shrink_800_800/0/1616413983355?e=2147483647&v=beta&t=egTafUuzkeMApWlRv9l7P5tb-K-8jALz77FEv06PZww",
         vibrate: [100,50,100],
         data: {
             dateOfArival: Date.now(),
